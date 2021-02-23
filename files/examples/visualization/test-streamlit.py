4bfcb20b5034d2eb8989be85ed2673e2b54f79de
Adam Lewis <alewis@qgpu2.lan>
2021-02-23 12:04:58 -0600

    make all visualizations work

---
+++ files/examples/visualization/test-streamlit.py
@@ -0,0 +1,25 @@
+import streamlit as st
+# To make things easier later, we're also importing numpy and pandas for
+# working with sample data.
+import numpy as np
+import pandas as pd
+
+st.title('My first app')
+
+st.write("Here's our first attempt at using data to create a table:")
+st.write(pd.DataFrame({
+    'first column': [1, 2, 3, 4],
+    'second column': [10, 20, 30, 40]
+}))
+
+"""
+# My first app
+Here's our first attempt at using data to create a table:
+"""
+
+df = pd.DataFrame({
+  'first column': [1, 2, 3, 4],
+  'second column': [10, 20, 30, 40]
+})
+
+df
