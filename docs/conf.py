6460c051e030a2a672c1e4d580df36a59cdcdd99
Chris Ostrouchov <chris.ostrouchov@gmail.com>
2021-03-02 11:40:58 -0500

    Adding automated docs

---
+++ docs/conf.py
@@ -0,0 +1,48 @@
+# -- Project information -----------------------------------------------------
+
+project = 'QHub OnPrem'
+copyright = '2021, Chris Ostrouchov'
+author = 'Chris Ostrouchov'
+
+# The full version, including alpha/beta/rc tags
+release = 'v0.1.0'
+
+# -- General configuration ---------------------------------------------------
+
+extensions = [
+    'recommonmark',
+    'sphinx_rtd_theme',
+]
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+# -- Options for HTML output -------------------------------------------------
+# https://github.com/bashtage/sphinx-material/
+
+html_theme = 'sphinx_material'
+html_title = 'QHub OnPrem'
+html_static_path = ['_static']
+
+html_theme_options = {
+    'nav_title': 'QHub OnPrem',
+
+    # Specify a base_url used to generate sitemap.xml. If not
+    # specified, then no sitemap will be built.
+    'base_url': 'https://project.github.io/project',
+
+    # Set the color and the accent color
+    'color_primary': '#652e8e',
+    'color_accent': '#32C574',
+
+    # Set the repo location to get a badge with stats
+    'repo_url': 'https://github.com/quansight/qhub-onprem/',
+    'repo_name': 'QHub OnPrem',
+
+    # Visible levels of the global TOC; -1 means unlimited
+    'globaltoc_depth': 3,
+    # If False, expand all TOC entries
+    'globaltoc_collapse': False,
+    # If True, show hidden TOC entries
+    'globaltoc_includehidden': False,
+}
