cad89d84dea315dbd12ce16485bf13ac23178d2f
Chris Ostrouchov <chris.ostrouchov@gmail.com>
2021-12-13 19:33:04 -0500

    Switch to using a flake

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "QHub HPC";
+
+  inputs = {
+    nixpkgs = { url = "github:nixos/nixpkgs/nixpkgs-unstable"; };
+  };
+
+  outputs = inputs@{ self, nixpkgs, ... }: {
+    devShell.x86_64-linux =
+      let
+        pkgs = import nixpkgs { system = "x86_64-linux"; };
+        pythonPackages = pkgs.python3Packages;
+      in pkgs.mkShell {
+        buildInputs = [
+          pkgs.docker-compose
+          pkgs.vagrant
+          pkgs.ansible
+
+          # development
+          pythonPackages.sphinx
+        ];
+      };
+  };
+}
